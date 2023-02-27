8d9c9ef4c4b443ca082e7fa4982cb0991debd125
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-27 14:57:18 +0100

    creazione prompt richiesta mail e array

---
+++ mail/js/main.js
@@ -0,0 +1,11 @@
+/*
+1) creare un'array con una serie di e-mail
+2) chiedere all'utente la sua e-mail
+3) se la sua mail è inserita anche nell'array consentirgli l'accesso altrimenti negarlo.
+4) stampare un messaggio con l'esito
+*/
+
+const yourMail = prompt("Inserisci la tua e-mail per effettuare l'accesso.")
+console.log(yourMail);
+
+const mailsAllowed = ['pippo.pluto@yahoo.it', 'topolino.pluto@gmail.com', 'paperino007@hotmail.it'];
