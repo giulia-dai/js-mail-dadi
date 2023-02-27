f91a85cdfab0a2bcfc4f9fd0e06fea941115bb9e
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-27 18:39:17 +0100

    calcolo media

---
+++ mail/bonus_media_numeri/js/main.js
@@ -0,0 +1,16 @@
+let media = [65, 29, 7, 10];
+console.log(media);
+
+let sommaMedia = false;
+
+for (let i = 0; i < media.length; i++) {
+
+    sommaMedia += media[i];
+
+}
+
+sommaMedia /= media.length;
+console.log(sommaMedia)
+
+
+
