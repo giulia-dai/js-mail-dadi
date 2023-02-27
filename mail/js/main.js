b55b6405c192caf7ff44096f56624023b81f1e05
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-27 15:21:31 +0100

    esercizio quasi funzionante

--- mail/js/main.js
+++ mail/js/main.js
@@ -5,7 +5,22 @@
 4) stampare un messaggio con l'esito
 */
 
-const yourMail = prompt("Inserisci la tua e-mail per effettuare l'accesso.")
+const yourMail = prompt("Inserisci la tua e-mail.")
 console.log(yourMail);
 
 const mailsAllowed = ['pippo.pluto@yahoo.it', 'topolino.pluto@gmail.com', 'paperino007@hotmail.it'];
+console.log(mailsAllowed);
+
+for (let i = 0; i < mailsAllowed.length; i++){
+    if (mailsAllowed[i] !== yourMail){
+
+        document.getElementById('login').innerHTML =`la tua e-mail: ${yourMail} non risulta nel nostro server. Accesso non consentito.`;
+        console.log(mailsAllowed[i]);
+
+    }else if (mailsAllowed[i] == yourMail){
+
+        document.getElementById('login').innerHTML =`la tua e-mail: ${yourMail}  risulta nel nostro server. Accesso  consentito.`;
+        console.log(mailsAllowed[i]);
+    }
+    
+}
